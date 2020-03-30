2a82babc41c21116c8c3f149b9c27bc101a00732
Nichita Postolachi <nikito888@gmail.com>
2020-03-30 23:52:17 +0100

    Fixed error 413 when uploading avatar

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var Live_Updates_Controller;
 //const xssFilters = require('xss-filters');
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 function back_to_main_thread(res, pdf_path) {
   res.json(pdf_path);
